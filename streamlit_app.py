7bff508553f68e7914d0e4eed761e8721789de06
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2025-12-17 11:21:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1052,8 +1052,8 @@ if monitoring_on:
         
         if not prediction_df.empty:
             display_df = prediction_df.copy()
-            display_df = display_df[['Odds', 'MoneyFlow', 'TotalFormScore', 'TotalScore']]
-            display_df.columns = ['當前賠率', '近期資金流(K)', '近績評分', '🔥綜合推薦分']
+            display_df = display_df[['馬名','Odds', 'MoneyFlow', 'TotalFormScore', 'TotalScore']]
+            display_df.columns = ['馬名','當前賠率', '近期資金流(K)', '近績評分', '🔥綜合推薦分']
             display_df['當前賠率'] = display_df['當前賠率'].apply(lambda x: f"{x:.1f}")
             display_df['近期資金流(K)'] = display_df['近期資金流(K)'].apply(lambda x: f"{x:.1f}")
             display_df['近績評分'] = display_df['近績評分'].astype(float).round(0).astype('Int64')
