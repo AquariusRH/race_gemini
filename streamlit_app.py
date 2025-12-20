5b4ae8956d5feba228f588490af0b147cee9733a
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2025-12-20 13:19:02 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -549,6 +549,8 @@ def change_overall(time_now):
 # ==================== 3. 繪圖函數 (簡化版) ====================
 def print_bar_chart(time_now):
   post_time = st.session_state.post_time_dict[race_no]
+  st.write(post_time)
+  st.write(time_now)  
   time_25_minutes_before = np.datetime64((post_time - timedelta(minutes=25)+timedelta(hours=8)).replace(tzinfo=None) )
   time_5_minutes_before = np.datetime64((post_time - timedelta(minutes=5)+timedelta(hours=8)).replace(tzinfo=None))
   
