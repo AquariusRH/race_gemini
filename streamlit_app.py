f73e644b2dc34331ecb24c5fe0f1f241e2eb9f93
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2026-02-11 13:07:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1375,7 +1375,7 @@ def print_henery_model(gamma=1.18):
         if not high_value_df.empty:
             st.dataframe(
                 high_value_df.style.background_gradient(subset=['Value'], cmap='Greens')
-                .format({"馬1單": "{:.1f}", "馬2單": "{:.1f}", "實時Q": "{:.1f}", "理論Q": "{:.1f}"}),
+                .format({"馬1獨贏": "{:.1f}", "馬2獨贏": "{:.1f}", "實時Q": "{:.1f}", "理論Q": "{:.1f}"}),
                 use_container_width=True, hide_index=True
             )
         else:
