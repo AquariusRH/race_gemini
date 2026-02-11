a1acf6fb22a18e4a9dae1fb0c7fff8aa1984c15c
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2026-02-12 00:23:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1374,7 +1374,7 @@ def print_henery_model(gamma=1.18):
                 st.table(
                     high_df.style.background_gradient(subset=['Value'], cmap='Greens')
                     .format({"馬1獨贏": "{:.1f}", "馬2獨贏": "{:.1f}", "實時Q": "{:.1f}", "理論Q": "{:.1f}", "Value": "{:.2f}"}),
-                    use_container_width=True, hide_index=True
+                    hide_index=True
                 )
             else:
                 st.info("目前無符合條件組合")
@@ -1386,7 +1386,7 @@ def print_henery_model(gamma=1.18):
                 st.table(
                     overheated_df.style.background_gradient(subset=['Value'], cmap='Reds_r')
                     .format({"馬1獨贏": "{:.1f}", "馬2獨贏": "{:.1f}", "實時Q": "{:.1f}", "理論Q": "{:.1f}", "Value": "{:.2f}"}),
-                    use_container_width=True, hide_index=True
+                   hide_index=True
                 )
             else:
                 st.info("目前無過熱組合")
