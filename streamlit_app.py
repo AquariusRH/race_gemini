dc3d09960eb1f071234ab5cdc6617262fbf34ed4
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2026-02-14 00:06:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -561,10 +561,7 @@ def fetch_horse_age_only(date_val, place_val, race_no):
             if response.status_code == 200:
                 soup = BeautifulSoup(response.text, 'html.parser')
                 st.write(soup)
-            return None
-
-    
-
+                return None
 
 def save_odds_data(time_now,odds):
   for method in methodlist:
