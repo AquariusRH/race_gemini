c3d30e9491b124b5f0b5f51f851408606e315b36
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2026-02-28 19:16:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1508,7 +1508,7 @@ def print_henery_model(gamma=1.18):
                 dragmode=False,
                 updatemenus=menu_list,
                 # 再次壓縮頂部邊距
-                margin=dict(t=50 + (row_count * 40), b=10, l=0, r=0), 
+                margin=dict(t=50 + (row_count * 40), b=0, l=0, r=0), 
                 height=500,
                 paper_bgcolor='rgba(0,0,0,0)',
                 font=dict(color="white", family="Arial")
