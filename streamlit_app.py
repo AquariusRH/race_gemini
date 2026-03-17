7c4992b25b34b17e8f685eb743dcb23328d97938
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2026-03-17 12:17:03 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1699,7 +1699,7 @@ def print_henery_model(gamma=1.18):
                     zauto=False,
                     colorbar=dict(title="過熱數")
                 ))
-                dynamic_height = 150 + (len(full_horse_list) * 45)
+                dynamic_height = 150 + (len(full_horse_list) * 35)
                 fig_heat.update_layout(
                     height=dynamic_height, # 動態高度
                     width = 1500,
